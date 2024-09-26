ae1471dafb0f65107d48ea651157e149f78270e8
Tyler <tylerm101104@gmail.com>
2024-09-26 10:34:31 +0100

    feat: timeline layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,28 @@
-import { Fragment } from "react";
+import styles from "./timeline.module.css";
+import Image from "next/image";
 
 export default function Page() {
-  return <Fragment>hello this is home</Fragment>;
+  return (
+    <div className={styles.timeline}>
+      <Image
+        className={styles.avatar}
+        src="https://i.pinimg.com/originals/04/7d/31/047d31ae8c50cf36a81927b14ebc948b.png"
+        width={200}
+        height={300}
+        alt="user avatar"
+      />
+      <div className={styles.column}>
+        <div className={styles.account}>Generic Name</div>
+        <div className={styles.content}>
+          Lorem ipsum dolor sit amet. Sit eius aliquid aut atque mollitia eum
+          placeat nihil ut alias itaque sed autem dolorem. Qui sunt vero vel
+        </div>
+        <div className={styles.reaction}>
+          <div>like</div>
+          <div>comment</div>
+          <div>share</div>
+        </div>
+      </div>
+    </div>
+  );
 }
