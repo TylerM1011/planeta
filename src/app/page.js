6ae7b3d339f2019c6d2955af259eda966a10c3cf
TylerM1011 <75587658+TylerM1011@users.noreply.github.com>
2024-10-06 16:24:02 +0100

    started making a new post input

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 "use client";
 
+import styles from "./timeline.module.css";
 import { useState } from "react";
 import TimelinePost from "@/components/TimelinePost";
+import IconButton from "@/components/IconButton";
 
-export default function Page() {
+export default function Page(props) {
   const [liked, setLiked] = useState(false);
   const [commenting, setCommenting] = useState(false);
   const [message, setMessage] = useState("");
@@ -15,15 +17,14 @@ export default function Page() {
   };
   return (
     <div>
-      <TimelinePost
-        liked={liked}
-        setLiked={setLiked}
-        setCommenting={setCommenting}
-        commenting={commenting}
-        onInputChange={(event) => setMessage(event.target.value)}
-        onSaveComment={saveComment}
-        comments={comments}
-      ></TimelinePost>
+      <div className={styles.newPost}>
+        <IconButton
+          width={32}
+          height={32}
+          unactiveImage="/icons/plus-small.svg"
+          onClick={() => TimelinePost}
+        ></IconButton>
+      </div>
       <TimelinePost
         liked={liked}
         setLiked={setLiked}
